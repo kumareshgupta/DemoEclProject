08a8efdcfb5f1141e93a5c1517b6c8a6e9e3af2f
Kumaresh Gupta <kumaresh_gupta@yahoo.com>
2020-06-01 11:35:46 +0530

    Created new class Wave

---
+++ Demo/src/com/kg/Wave.java
@@ -0,0 +1,14 @@
+package com.kg;
+
+public class Wave {
+	private int intensity;
+
+	public int getIntensity() {
+		return intensity;
+	}
+
+	public void setIntensity(int intensity) {
+		this.intensity = intensity;
+	}
+	
+}
